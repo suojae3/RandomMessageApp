ad35185dbd7d84226ce84d1edd22792148dfd886
suojae3 <suojae3@gmail.com>
2023-09-18 14:01:52 +0900

    add floating button animation

--- RandomMessageApp/MainViewController/MainViewController.swift
+++ RandomMessageApp/MainViewController/MainViewController.swift
@@ -8,12 +8,39 @@ import Starscream
 //MARK: - Properties
 class MainViewController: UIViewController {
     
-    private lazy var floatingButtonVC: FloatingButtonViewController = {
-        let controller = FloatingButtonViewController()
-        addChild(controller)
-        return controller
+    
+    private lazy var floatingButton: UIButton = {
+        let button = UIButton()
+        var config = UIButton.Configuration.filled()
+        config.baseBackgroundColor = .systemPink
+        config.cornerStyle = .capsule
+        config.image = UIImage(systemName: "plus")?.withConfiguration(UIImage.SymbolConfiguration(pointSize: 20, weight: .medium))
+        button.configuration = config
+        button.layer.shadowRadius = 10
+        button.layer.shadowOpacity = 0.3
+        button.addTarget(self, action: #selector(didTapFloatingButton), for: .touchUpInside)
+        return button
     }()
-
+    private let writeButton: UIButton = {
+        let button = UIButton()
+        var config = UIButton.Configuration.filled()
+        config.baseBackgroundColor = .systemPink
+        config.cornerStyle = .capsule
+        config.image = UIImage(systemName: "pencil")?.withConfiguration(UIImage.SymbolConfiguration(pointSize: 20, weight: .medium))
+        button.configuration = config
+        button.layer.shadowRadius = 10
+        button.layer.shadowOpacity = 0.3
+        button.alpha = 0.0
+        return button
+    }()
+    private var isActive: Bool = false {
+        didSet {
+            showActionButtons()
+        }
+    }
+    private var animation: UIViewPropertyAnimator?
+    
+    
 }
 
 
@@ -30,6 +57,12 @@ extension MainViewController {
         
         
     }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        floatingButton.frame = CGRect(x: view.frame.size.width - 60 - 8 - 20, y: view.frame.size.height - 60 - 8 - 40, width: 60, height: 60)
+        writeButton.frame = CGRect(x: view.frame.size.width - 60 - 8 - 20, y: view.frame.size.height - 60 - 80 - 8 - 40, width: 60, height: 60)
+    }
 }
 
 
@@ -38,9 +71,10 @@ extension MainViewController {
 extension MainViewController {
     
     func setupUI() {
-        view.addSubview(floatingButtonVC.view)
-        floatingButtonVC.didMove(toParent: self)
-
+        view.addSubview(floatingButton)
+        view.addSubview(writeButton)
+        
+        
         setupConstraints()
         
     }
@@ -49,16 +83,58 @@ extension MainViewController {
 //MARK: - Constraints
 extension MainViewController {
     func setupConstraints() {
-        let floatingButtonView = floatingButtonVC.view!
         
-        floatingButtonView.snp.makeConstraints { make in
+        floatingButton.snp.makeConstraints { make in
             make.right.equalToSuperview().offset(-20)
             make.bottom.equalToSuperview().offset(-40)
             make.width.equalTo(60)
             make.height.equalTo(60)
         }
-
+        
     }
+}
+
+//MARK: - Button Action
+extension MainViewController {
+    
+    @objc private func didTapFloatingButton() {
+        isActive.toggle()
+    }
+    
+    private func showActionButtons() {
+        popButtons()
+        rotateFloatingButton()
+    }
+    
+    private func popButtons() {
+        if isActive {
+            writeButton.layer.transform = CATransform3DMakeScale(0.4, 0.4, 1)
+            UIView.animate(withDuration: 0.3, delay: 0.2, usingSpringWithDamping: 0.55, initialSpringVelocity: 0.3, options: [.curveEaseInOut], animations: { [weak self] in
+                guard let self = self else { return }
+                self.writeButton.layer.transform = CATransform3DIdentity
+                self.writeButton.alpha = 1.0
+            })
+        } else {
+            UIView.animate(withDuration: 0.15, delay: 0.2, options: []) { [weak self] in
+                guard let self = self else { return }
+                self.writeButton.layer.transform = CATransform3DMakeScale(0.4, 0.4, 0.1)
+                self.writeButton.alpha = 0.0
+            }
+        }
+    }
+    
+    private func rotateFloatingButton() {
+        let animation = CABasicAnimation(keyPath: "transform.rotation.z")
+        let fromValue = isActive ? 0 : CGFloat.pi / 4
+        let toValue = isActive ? CGFloat.pi / 4 : 0
+        animation.fromValue = fromValue
+        animation.toValue = toValue
+        animation.duration = 0.3
+        animation.fillMode = .forwards
+        animation.isRemovedOnCompletion = false
+        floatingButton.layer.add(animation, forKey: nil)
+    }
+
 }
 
 //MARK: - Preview
